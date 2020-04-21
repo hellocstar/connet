33ebf63db6aaa9fbaa025b770e1a25a370ee6a37
helloallstar <chanchunyat1999@gmail.com>
2020-04-21 15:58:52 +0800

    after tommy

--- back/api/roomApi.js
+++ back/api/roomApi.js
@@ -31,6 +31,11 @@ const createRoom = async (req, res, next) => {
 	try {
 		const room = new Room(req.body);
 		const result = await room.save();
+		const updateHistory = await Profile.findOneAndUpdate(
+			{ _id: room.hostID },
+			{ $push: { history: room._id } }
+		);
+		console.log(updateHistory);
 		res.status(201).send(result._id);
 	} catch (e) {
 		res.status(500).send(e);
@@ -96,8 +101,8 @@ const updateRoom = async (req, res, next) => {
 const deleteRoom = async (req, res, next) => {
 	try {
 		const result = await Room.deleteOne({ name: req.params.roomname });
-		if (result.deletedCount === 0) res.status(404).send();
-		else res.status(200).send('Deleted Successfully');
+		// if (result.deletedCount === 0) res.status(404).send();
+		res.status(200).send('Deleted Successfully');
 	} catch (e) {
 		res.status(500).send(e);
 	}
