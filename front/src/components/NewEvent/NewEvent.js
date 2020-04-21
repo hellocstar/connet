73a902eac240772eaa12badaa6c969c425779827
ecxcxcx <ecxcxcx@gmail.com>
2020-04-21 16:31:55 +0800

    added photo upload field

--- front/src/components/NewEvent/NewEvent.js
+++ front/src/components/NewEvent/NewEvent.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import { Select } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import MenuItem from '@material-ui/core/MenuItem'
-import Input from '@material-ui/core/Input'
-import Chip from '@material-ui/core/Chip'
-import Button from '@material-ui/core/Button'
+import React, { useState, useEffect } from "react";
+import { Select } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import Input from "@material-ui/core/Input";
+import Chip from "@material-ui/core/Chip";
+import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
     maxWidth: 300,
   },
   chips: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   chip: {
     margin: 2,
@@ -22,41 +22,41 @@ const useStyles = makeStyles(theme => ({
   noLabel: {
     marginTop: theme.spacing(3),
   },
-}))
+}));
 
 const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
-  const classes = useStyles()
+  const classes = useStyles();
 
-  const [name, setName] = useState('')
-  const [date, setDate] = useState('')
-  const [time, setTime] = useState('')
-  const [location, setLocation] = useState('')
-  const [description, setDescription] = useState('')
-  const [photo, setPhoto] = useState('')
-  const [categories, setCategories] = useState([])
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [categories, setCategories] = useState([]);
 
   if (isSignedIn) {
     const categoriesList = [
-      'Outdoors & Adventure',
-      'Tech',
-      'Family',
-      'Health & Wellness',
-      'Sports & Fitness',
-      'Learning',
-      'Photography',
-      'Food & Drink',
-      'Writing',
-      'Language & Culture',
-      'Music',
-      'Film',
-      'Beliefs',
-      'Arts',
-      'Fashion & Beauty',
-      'Career & Business',
-    ]
+      "Outdoors & Adventure",
+      "Tech",
+      "Family",
+      "Health & Wellness",
+      "Sports & Fitness",
+      "Learning",
+      "Photography",
+      "Food & Drink",
+      "Writing",
+      "Language & Culture",
+      "Music",
+      "Film",
+      "Beliefs",
+      "Arts",
+      "Fashion & Beauty",
+      "Career & Business",
+    ];
 
-    const ITEM_HEIGHT = 48
-    const ITEM_PADDING_TOP = 8
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
     const MenuProps = {
       PaperProps: {
         style: {
@@ -64,13 +64,13 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
           width: 250,
         },
       },
-    }
+    };
 
     const onSubmit = () => {
-      fetch('http://localhost:3000/newevent', {
-        method: 'post',
+      fetch("http://localhost:3000/newevent", {
+        method: "post",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           name: name,
@@ -79,37 +79,37 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
           location: location,
           organiserID: user.id,
           description: description,
-          //photo: photo,
+          photo: photo,
           categories: categories,
         }),
       })
-        .then(response => response.json())
-        .then(id => {
+        .then((response) => response.json())
+        .then((id) => {
           if (id) {
-            onActivityIDChange(id)
-            onRouteChange('event/' + id)
+            onActivityIDChange(id);
+            onRouteChange("event/" + id);
           }
-        })
-    }
+        });
+    };
 
-    const handleImageUpload = event => {
-      console.log(event)
-      const files = event.target.files
-      const formData = new FormData()
-      formData.append('eventPhoto', files[0])
-      const file = fetch('/event/photo', {
-        method: 'POST',
+    const handleImageUpload = (event) => {
+      console.log(event);
+      const files = event.target.files;
+      const formData = new FormData();
+      formData.append("eventPhoto", files[0]);
+      const file = fetch("/event/photo", {
+        method: "POST",
         body: formData,
       })
-        .then(response => response.formData())
-        .then(data => {
-          console.log(data)
+        .then((response) => response.formData())
+        .then((data) => {
+          console.log(data);
         })
-        .catch(error => {
-          console.error(error)
-        })
-      setPhoto(file)
-    }
+        .catch((error) => {
+          console.error(error);
+        });
+      setPhoto(file);
+    };
 
     return (
       <div>
@@ -121,16 +121,16 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
               type="text"
               name="name"
               id="name"
-              onChange={event => setName(event.target.value)}
+              onChange={(event) => setName(event.target.value)}
             />
           </div>
           <div>
-            {/* <input
+            <input
               id="contained-button-file"
               multiple
               type="file"
-              onChange={event => handleImageUpload(event, event.target.file)}
-            /> */}
+              onChange={(event) => handleImageUpload(event, event.target.file)}
+            />
             {/* <label htmlFor="contained-button-file">
               <Button
                 variant="raised"
@@ -147,7 +147,7 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
               type="date"
               name="date"
               id="date"
-              onChange={event => setDate(event.target.value)}
+              onChange={(event) => setDate(event.target.value)}
             />
           </div>
           <div>
@@ -156,7 +156,7 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
               type="time"
               name="time"
               id="time"
-              onChange={event => setTime(event.target.value)}
+              onChange={(event) => setTime(event.target.value)}
             />
           </div>
           <div>
@@ -165,7 +165,7 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
               type="text"
               name="location"
               id="location"
-              onChange={event => setLocation(event.target.value)}
+              onChange={(event) => setLocation(event.target.value)}
             />
           </div>
           <div>
@@ -174,7 +174,7 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
               type="text"
               name="description"
               id="description"
-              onChange={event => setDescription(event.target.value)}
+              onChange={(event) => setDescription(event.target.value)}
             />
           </div>
           {/* <div>
@@ -192,18 +192,18 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
               id="demo-mutiple-chip"
               multiple
               value={categories}
-              onChange={event => setCategories(event.target.value)}
+              onChange={(event) => setCategories(event.target.value)}
               input={<Input id="select-multiple-chip" />}
-              renderValue={selected => (
+              renderValue={(selected) => (
                 <div className={classes.chips}>
-                  {selected.map(value => (
+                  {selected.map((value) => (
                     <Chip key={value} label={value} className={classes.chip} />
                   ))}
                 </div>
               )}
               MenuProps={MenuProps}
             >
-              {categoriesList.map(cat => (
+              {categoriesList.map((cat) => (
                 <MenuItem key={cat} value={cat}>
                   {cat}
                 </MenuItem>
@@ -215,14 +215,14 @@ const NewEvent = ({ onActivityIDChange, onRouteChange, isSignedIn, user }) => {
           <input onClick={onSubmit} type="submit" value="Create!" />
         </div>
       </div>
-    )
+    );
   } else {
     return (
       <div>
         <p>Sign In or Sign Up to do whatever you want!</p>
       </div>
-    )
+    );
   }
-}
+};
 
-export default NewEvent
+export default NewEvent;
